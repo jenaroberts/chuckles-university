48f3870a3fa1f404dc391b0948b6f1221c23539f
Jena Roberts <jenaroberts73@gmail.com>
2022-07-10 21:28:30 -0400

    setting up theme with mui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline } from "@mui/material";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: `monospace`,
+    fontSize: 28,
+  },
+  palette: {
+    mode: "light",
+    background: {
+      default: "#00acc1",
+    },
+    primary: {
+      main: "#5ddef4",
+    },
+    secondary: {
+      main: "#007c91",
+    },
+  },
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App"></div>
+    </ThemeProvider>
   );
 }
 
